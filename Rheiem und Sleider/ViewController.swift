747a9d77c6e576bd5460a62777b8bfc382333474
codermaz <yilmazadem@gmail.com>
2017-11-05 16:47:47 +0100

    v2

--- Rheiem und Sleider/ViewController.swift
+++ Rheiem und Sleider/ViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String( (Int (sliderOutlet.value)) * (indexPath.row + 1) )
+        return cell
+    }
+    
 
+    @IBOutlet weak var sliderOutlet: UISlider!
+    
+    
+    @IBOutlet weak var tableOutlet: UITableView!
+    
+    @IBAction func sliderAction(_ sender: Any) {
+        tableOutlet.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
